9b0b2119719886686d6470536c13fedd46a18cfe
paparuch <paparuch@0day.im>
2022-11-27 02:37:36 +0000

    Added components

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -42,30 +42,31 @@ contract Marketplace {
     function createProduct(string memory p_name, uint p_price,string memory p_description, string memory p_photo) public {
         require(bytes(p_name).length > 0);
         require(p_price > 0);
-        require(bytes(p_description) > 0);
-        require(bytes(p_photo) > 0);
+        require(bytes(p_description).length > 0);
+        require(bytes(p_photo).length > 0);
         
         productsCount++;
-        products[productCount] = Product(productCount, p_name, p_price, msg.sender, false, p_description, p_photo);
-        emit ProductCreated(productCount, p_name, p_price, msg.sender, false, p_description, p_photo);
+        Product memory product = Product(productsCount, p_name, p_price, msg.sender, false, p_description, p_photo);
+        products[productsCount] = product;
+        emit ProductCreated(product.m_id, product.m_name, product.m_price, product.m_owner, product.m_purchased, product.m_description, product.m_photo);
     }
     
     function purchaseProduct(uint p_id) public payable {
         Product memory p_product = products[p_id];
-        require(p_product.id > 0);
-        require(p_product.id <= productsCount);
-        require(msg.value >= p_product.price);
-        require(!p_product.purchased);
+        require(p_product.m_id > 0);
+        require(p_product.m_id <= productsCount);
+        require(msg.value >= p_product.m_price);
+        require(!p_product.m_purchased);
         
-        address payable p_seller = p_product.owner;
+        address payable p_seller = p_product.m_owner;
         
         require(p_seller != msg.sender);
-        p_product.owner = msg.sender;
-        p_product.purchased = true;
+        p_product.m_owner = msg.sender;
+        p_product.m_purchased = true;
         products[p_id] = p_product;
         
         address(p_seller).transfer(msg.value);
         
-        emit ProductPurchased(productCount, p_product.name, p_product.price, msg.sender, true, );
+        emit ProductPurchased(p_id, p_product.m_name, p_product.m_price, p_product.m_owner, p_product.m_purchased, p_product.m_description, p_product.m_photo);
     }
 }
